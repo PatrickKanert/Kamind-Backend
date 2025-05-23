2a5efcf86192fe55786f08d3d275295b32c1e9bd
PatrickKanert <patrickkanert@gmail.com>
2025-05-23 12:40:27 +0200

    Add permission checks to TaskViewSet and TaskCommentView for enhanced security

--- tasks_app/api/views.py
+++ tasks_app/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from tasks_app.models import Task, Comment
 from .serializers import TaskListSerializer, TaskSerializer, CommentSerializer
+from rest_framework.exceptions import PermissionDenied
 
 class AssignedTasksView(APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -31,6 +32,20 @@ class TaskViewSet(viewsets.ModelViewSet):
         if self.action in ['list', 'retrieve']:
             return TaskListSerializer
         return TaskSerializer
+    
+    def get_object(self):
+        obj = super().get_object()
+        user = self.request.user
+        board = obj.board
+
+        if not (
+            obj.created_by == user or
+            board.owner == user or
+            user in board.members.all()
+        ):
+            raise PermissionDenied("You do not have permission to access this task.")
+
+        return obj
 
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
@@ -38,7 +53,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     def destroy(self, request, *args, **kwargs):
         task = self.get_object()
         if task.created_by != request.user and task.board.owner != request.user:
-            return Response({'detail': 'Nicht erlaubt'}, status=status.HTTP_403_FORBIDDEN)
+            return Response({'detail': 'Not allowed'}, status=status.HTTP_403_FORBIDDEN)
         return super().destroy(request, *args, **kwargs)
 
 class TaskCommentView(APIView):
@@ -46,16 +61,37 @@ class TaskCommentView(APIView):
 
     def get(self, request, task_id):
         task = get_object_or_404(Task, id=task_id)
+        user = request.user
+        board = task.board
+
+        if not (
+            task.created_by == user or
+            board.owner == user or
+            user in board.members.all()
+        ):
+            raise PermissionDenied("You do not have permission to view comments on this task.")
+
         comments = task.comments.all().order_by('created_at')
         serializer = CommentSerializer(comments, many=True)
         return Response(serializer.data)
 
     def post(self, request, task_id):
         task = get_object_or_404(Task, id=task_id)
+        user = request.user
+        board = task.board
+
+        if not (
+            task.created_by == user or
+            board.owner == user or
+            user in board.members.all()
+        ):
+            raise PermissionDenied("You do not have permission to comment on this task.")
+
         content = request.data.get('content')
         if not content:
-            return Response({'detail': 'Content ist erforderlich.'}, status=400)
-        comment = Comment.objects.create(task=task, author=request.user, content=content)
+            return Response({'detail': 'Content is required.'}, status=400)
+
+        comment = Comment.objects.create(task=task, author=user, content=content)
         serializer = CommentSerializer(comment)
         return Response(serializer.data, status=201)
 
@@ -65,6 +101,6 @@ class CommentDeleteView(APIView):
     def delete(self, request, task_id, comment_id):
         comment = get_object_or_404(Comment, id=comment_id, task_id=task_id)
         if comment.author != request.user:
-            return Response({'detail': 'Nicht erlaubt'}, status=403)
+            return Response({'detail': 'Not allowed'}, status=403)
         comment.delete()
         return Response(status=204)
